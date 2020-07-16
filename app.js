60663a81763cfc0b13c6060764b203fec85c2844
Ryan Williams <ryan.williams@fistech.group>
2020-07-15 23:27:42 -0500

    add: prettierrc

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello from my node server!");
- }
-);
+  res.send("Hello from my node server!");
+});
 
 app.get("/test", (req, res) => {
-    res.status(418);
-    res.set('x-test-status', 'test-fail');
-    res.send("this isn't working!");
-  }
-);
+  res.status(418);
+  res.set("x-test-status", "test-fail");
+  res.send("this isn't working!");
+});
 
 app.listen(port, () => console.log(`Example app running on port ${port}`));
